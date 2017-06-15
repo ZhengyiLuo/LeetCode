6b09f700674d0b4abe906db96724e15cb1d9e6ef
ZhengyiLuo <zhengyiluo5@gmail.com>
2017-06-15 02:37:54 +0000

    finished partition, need testing

---
+++ main.py
@@ -0,0 +1,13 @@
+import Utils
+
+if __name__ == "__main__":
+  print("Hell World")
+  utils = Utils.Utils()
+#  print(utils.binarySearch([1,2,3,4,5], 5))
+  print(utils.countSort([1,2,3,4,5], 5))
+
+  
+  
+
+
+
